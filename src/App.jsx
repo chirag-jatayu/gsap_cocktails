35acfaaab9b1f7064007382477de87cee43245fa
chirag-jatayu <chirag.jatayu@gmail.com>
2025-06-27 19:20:08 +0530

    feat: add Cocktails component with GSAP animations and responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import gsap from 'gsap'
 import { ScrollTrigger,SplitText } from 'gsap/all'
 import Navbar from './components/Navbar'
 import Hero from './components/hero'
+import Cocktails from './components/Cocktails'
 gsap.registerPlugin(ScrollTrigger,SplitText)
 const App = () => {
   return (
     <main>
       <Navbar/>
       <Hero/>
+      <Cocktails/>
       {/* <div className='h-dvh bg-black'></div> */}
     </main>
   )
