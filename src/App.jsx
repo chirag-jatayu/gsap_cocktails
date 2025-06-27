dab9f2fc83b2b9438d8fce648bd2c49daec71b77
chirag-jatayu <chirag.jatayu@gmail.com>
2025-06-27 15:29:31 +0530

    feat: initialize React project with GSAP and Tailwind CSS
    - Added package.json with dependencies for React, GSAP, and Tailwind CSS.
    - Included various assets such as images, fonts, and videos in the public directory.
    - Created main application structure with App, Navbar, and Hero components.
    - Implemented GSAP animations in Navbar and Hero components.
    - Defined navigation links and cocktail lists in constants.
    - Set up global styles with Tailwind CSS and custom CSS utilities.
    - Configured Vite for development and build processes.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import gsap from 'gsap'
+import { ScrollTrigger,SplitText } from 'gsap/all'
+import Navbar from './components/Navbar'
+import Hero from './components/hero'
+gsap.registerPlugin(ScrollTrigger,SplitText)
+const App = () => {
+  return (
+    <main>
+      <Navbar/>
+      <Hero/>
+    </main>
+  )
+}
+
+export default App
